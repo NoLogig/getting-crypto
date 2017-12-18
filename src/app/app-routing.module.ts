5db0e7d69637987ba89bff3b55eff0d6ada6b575
NoLogig <max-schweigerdt@web.de>
2017-12-18 14:16:36 +0100

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', children: [], component: AppComponent },
+
+  { path: '**', component: AppComponent   /* to Do: Fancy PageNotFound 8) */ }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
