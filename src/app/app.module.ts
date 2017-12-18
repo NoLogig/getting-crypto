428154245c9f722946c01e45ceba20c453663734
NoLogig <max-schweigerdt@web.de>
2017-12-18 20:10:50 +0100

    init basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
 
+// Materials
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatListModule, MatCheckboxModule, MatCardModule, MatToolbarModule, MatTabsModule,
+  MatButtonToggleModule, MatExpansionModule, MatFormFieldModule, MatDatepickerModule,
+  MatNativeDateModule, MatSlideToggleModule, MatSortModule, MatIconModule, MatSidenavModule,
+  MatButtonModule, MatInputModule, MatSelectModule
+} from '@angular/material';
+import 'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpModule,
+    MatSortModule, MatCheckboxModule, MatSlideToggleModule, MatCardModule, MatNativeDateModule,
+    MatListModule, MatButtonModule, MatExpansionModule, MatFormFieldModule, MatInputModule, MatDatepickerModule,
+    MatIconModule, MatToolbarModule, MatTabsModule, MatSidenavModule, MatButtonToggleModule, MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
